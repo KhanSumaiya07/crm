dad1855a1d5739405774949d5410857f0e9b821a
Pranshu Gupta <111183216+pranshugp@users.noreply.github.com>
2025-06-16 15:28:51 +0530

    add lead edit

---
+++ app/api/leads/[id]/route.js
@@ -0,0 +1,24 @@
+import { connectDB } from "../../../../../lib/db";
+import Lead from "../../../../../models/Lead";
+
+export async function GET(req, { params }) {
+  await connectDB();
+  const lead = await Lead.findById(params.id);
+  if (!lead) return new Response("Not found", { status: 404 });
+  return new Response(JSON.stringify(lead), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export async function PUT(req, { params }) {
+  await connectDB();
+  const body = await req.json();
+  const updatedLead = await Lead.findByIdAndUpdate(params.id, body, {
+    new: true,
+  });
+  return new Response(JSON.stringify(updatedLead), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
