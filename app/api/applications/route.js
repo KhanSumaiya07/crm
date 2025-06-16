38339fe4f2792973506c07154ac261e6775dc6f6
Pranshu Gupta <111183216+pranshugp@users.noreply.github.com>
2025-06-16 15:08:44 +0530

    add application backend

---
+++ app/api/applications/route.js
@@ -0,0 +1,19 @@
+import { connectDB } from "../../../lib/db";
+import Application from "../../../models/Applications";
+import Lead from "../../../../models/Lead";
+
+import { v4 as uuidv4 } from 'uuid';
+
+export async function POST(req) {
+  await connectDB();
+  const body = await req.json();
+  body.referenceNo = uuidv4();
+  const app = await Application.create(body);
+  return new Response(JSON.stringify(app), { status: 201 });
+}
+
+export async function GET() {
+  await connectDB();
+  const apps = await Application.find().populate('lead');
+  return new Response(JSON.stringify(apps), { status: 200 });
+}
